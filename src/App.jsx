48636b977a4df834e3e485fd1367535815d688c2
Thomas Roberts <thomas@becausemarket.com>
2023-11-18 13:15:44 -0500

    remove styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+export const product = (props) => {
+  alert('From product')
+  console.log('Product function called!');
+};
+
+
 function App() {
   const [count, setCount] = useState(0)
+
 
   return (
     <>
