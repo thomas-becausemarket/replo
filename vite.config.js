2c5ec2adb6b60f54f7c0cdcd0f0a0a8e45c0f873
Thomas Roberts <thomas@becausemarket.com>
2023-11-20 14:50:41 -0500

    remove console

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/replo/"
+  base: "/replo/",
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: 'assets/bundle.js', // Specify the desired output file name
+      }
+    }
+  }
 })
